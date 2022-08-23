233c34390ae578f36190e047d8eb70058e533357
Maks <mazda4697@gmail.com>
2022-08-23 14:46:29 +0300

    fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import sys
-from cli import welcome_user
 
-def welcome(hello):
-    print(hello)
+from brain_games.cli import welcome_user
+
 
 def main():
-    welcome("Welcome to the Brain Games!")
-    welcome_user
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
