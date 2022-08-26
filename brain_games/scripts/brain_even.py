aa94c0b1bd3d258728a93a746127539b7c9dce62
Maks <mazda4697@gmail.com>
2022-08-26 13:43:37 +0300

    add brain_even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,44 @@
+import random
+import prompt
+
+def welcome():
+    print('Welcome to the Brain Games!')
+
+def yours_name():
+    return prompt.string('May I have your name? ')
+
+def hello(name):
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+
+def is_even_number(num):
+    return num % 2 == 0
+    
+def logic_answer(answer, num):
+    if(is_even_number(num) == True and answer == "yes"):
+        return True
+    elif(is_even_number(num) == False and answer == "no"):
+        return True
+    
+    
+def game(name):
+    hello(name)
+    for i in range(3):
+        num = random.randint(1, 100)
+        print("Question: ", num)
+        answer = input("Your answer: ")
+        result = False
+        result = logic_answer(answer, num)
+        if(result != True ):
+            return "'yes' is wrong answer ;(. Correct answer was 'no'." + '\n' + "Let's try again"
+        else:
+            print("Correct!")
+    return "Congratulations, " + name
+    
+def main():
+    welcome()
+    print(game(yours_name()))
+    
+    
+if __name__ == '__main__':
+    main()
