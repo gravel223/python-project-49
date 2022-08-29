7262e08e78ba43142e5155e676e0e82a2bc2f2e2
Maks <mazda4697@gmail.com>
2022-08-29 15:07:19 +0300

    add logic gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,35 @@
+from brain_games.scripts.brain_calc import random_nember
+from brain_games.scripts.brain_even import welcome
+from brain_games.scripts.brain_even import yours_name
+
+def greatest_common_divisor(a, b):
+    while a != 0 and b != 0:
+        if(a > b):
+            a = a % b
+        else:
+            b = b % a
+    return(a + b)
+        
+def game(name):
+    print("Find the greatest common divisor of given numbers.")
+    for i in range(0, 3):
+        num1 = random_nember()
+        num2 = random_nember()
+        print(f"Question: {num1} {num2}")
+        answer = int(input("Your answer: "))
+        result = greatest_common_divisor(num1, num2)
+        if(answer == result):
+            print("Correct!")
+        else:
+            return f"{answer}  is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}"
+    return f"Congratulations, {name}"        
+
+
+def main():
+    welcome()
+    print(game(yours_name()))
+
+    
+
+if __name__ == '__main__':
+    main()
