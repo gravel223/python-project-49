f3cd0bcea86b288c963d7a775515806a1ff4f24c
Maks <mazda4697@gmail.com>
2022-08-30 13:13:17 +0300

    add logic progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,37 @@
+from brain_games.scripts.brain_even import welcome
+from brain_games.scripts.brain_even import yours_name
+import random
+ 
+def create_geometric_progression():
+    begin = random.randint(1, 150)
+    q = random.randint(1, 5)
+    return [begin+i*q for i in range(1, 10)]
+    
+        
+        
+        
+
+def game(name):
+    print("What number is missing in the progression?")
+    for i in range(3):
+        progression = create_geometric_progression()
+        question = random.randint(0, 9)
+        result = progression[question]
+        progression.remove(result)
+        progression.insert(question, "..")
+        print(f"Question: {progression}")
+        answer = input("Your answer: ")
+        if(answer.isdigit() == True and int(answer) == result):
+            print("Correct!")
+        else:
+            return f"{answer}  is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}"
+    return f"Congratulations, {name}!" 
+        
+
+
+def main():
+    welcome()
+    print(game(yours_name()))
+
+if __name__ == '__main__':
+    main()
