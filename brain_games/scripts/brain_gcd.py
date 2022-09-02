161b619729e61f76a2fe0d7ce99a3bbfadc55e8c
Maks <mazda4697@gmail.com>
2022-09-02 15:16:54 +0300

    flake8 fix gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,13 +5,13 @@ from brain_games.scripts.brain_even import yours_name
 
 def greatest_common_divisor(a, b):
     while a != 0 and b != 0:
-        if(a > b):
+        if (a > b):
             a = a % b
         else:
             b = b % a
-    return(a + b)
+    return (a + b)
 
-       
+
 def game(name):
     print("Find the greatest common divisor of given numbers.")
     for i in range(0, 3):
@@ -20,18 +20,18 @@ def game(name):
         print(f"Question: {num1} {num2}")
         answer = input("Your answer: ")
         result = greatest_common_divisor(num1, num2)
-        if(answer.isdigit() == True and int(answer) == result):
+        if (answer.isdigit() is True and int(answer) == result):
             print("Correct!")
         else:
-            return f"{answer}  is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}!"
-    return f"Congratulations, {name}!"       
+            print(f"{answer}  is wrong answer ;(. Correct answer was {result}")
+            return f"Let's try again, {name}!"
+    return f"Congratulations, {name}!"
 
 
 def main():
     welcome()
     print(game(yours_name()))
 
-    
 
 if __name__ == '__main__':
     main()
