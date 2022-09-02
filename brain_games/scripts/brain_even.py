df46966ad1b66220f5254719e693f49a5c0c050d
Maks <mazda4697@gmail.com>
2022-09-02 15:04:23 +0300

    flake8 fix even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,10 @@
 import random
 import prompt
 
+
 def welcome():
     print('Welcome to the Brain Games!')
+
 
 def yours_name():
     name = prompt.string('May I have your name? ')
@@ -12,14 +14,15 @@ def yours_name():
 
 def is_even_number(num):
     return num % 2 == 0
-    
+
+
 def logic_answer(answer, num):
-    if(is_even_number(num) == True and answer == "yes"):
+    if (is_even_number(num) is True and answer == "yes"):
         return True
-    elif(is_even_number(num) == False and answer == "no"):
+    elif (is_even_number(num) is False and answer == "no"):
         return True
-    
-    
+
+
 def game(name):
     print('Answer "yes" if the number is even, otherwise answer "no"')
     for i in range(3):
@@ -28,16 +31,18 @@ def game(name):
         answer = input("Your answer: ")
         result = False
         result = logic_answer(answer, num)
-        if(result == True ):
+        if (result is True):
             print("Correct!")
         else:
-            return f"{answer}  is wrong answer ;(. Correct answer was 'no' \n Let's try again, {name}!"
+            print(f"{answer}  is wrong answer ;(. Correct answer was {result}")
+            return f"Let's try again, {name}!"
     return f"Congratulations, {name}!"
-    
+
+
 def main():
     welcome()
     print(game(yours_name()))
-    
-    
+
+
 if __name__ == '__main__':
     main()
