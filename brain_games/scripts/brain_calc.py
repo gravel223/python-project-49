de1e87b64a9b2e52d504eb3888c5b6a02c5b2bd2
Maks <mazda4697@gmail.com>
2022-09-02 14:38:12 +0300

    flake8 fix calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,12 +2,15 @@ from brain_games.scripts.brain_even import welcome
 from brain_games.scripts.brain_even import yours_name
 import random
 
+
 def random_nember():
     return random.randint(1, 15)
+
 
 def operators():
     operators = ("+", "*")
     return operators[random.randint(0, 1)]
+
 
 def game(name):
     print("What is the result of the expression?")
@@ -18,28 +21,22 @@ def game(name):
         result = 0
         print("Question: " + str(num_one) + " " + operator + " " + str(num_two))
         answer = input("Your answer: ")
-        if(operator == "+"):
+        if (operator == "+"):
             result = num_one + num_two
         else:
             result = num_one * num_two
-        if(answer.isdigit() == True and int(answer) == result):
+        if (answer.isdigit() is True and int(answer) == result):
             print("Correct!")
         else:
-            return f"{answer}  is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}!"
-    return f"Congratulations, {name}!" 
-        
-    
-    
+            print(f"{answer}  is wrong answer ;(. Correct answer was {result}")
+            return f"Let's try again, {name}!"
+    return f"Congratulations, {name}!"
 
 
 def main():
     welcome()
     print(game(yours_name()))
-    
-    
-    
-    
-    
+
+
 if __name__ == '__main__':
     main()
-    
