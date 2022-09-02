45cd8a1941b2077d7c5ef766e79fd8bf6f6e8254
Maks <mazda4697@gmail.com>
2022-09-02 15:45:41 +0300

    flake8 fix progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,15 +1,13 @@
 from brain_games.scripts.brain_even import welcome
 from brain_games.scripts.brain_even import yours_name
 import random
- 
+
+
 def create_geometric_progression():
     begin = random.randint(1, 150)
     q = random.randint(1, 5)
-    return [str(begin+i*q) for i in range(10)]
-    
-        
-        
-        
+    return [str(begin + i * q) for i in range(10)]
+
 
 def game(name):
     print("What number is missing in the progression?")
@@ -21,17 +19,18 @@ def game(name):
         progression.insert(question, '..')
         print("Question: " + ' '.join(progression))
         answer = input("Your answer: ")
-        if(answer.isdigit() == True and int(answer) == int(result)):
+        if (answer.isdigit() is True and int(answer) == int(result)):
             print("Correct!")
         else:
-            return f"{answer}  is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}!"
-    return f"Congratulations, {name}!" 
-        
+            print(f"{answer}  is wrong answer ;(. Correct answer was {result}")
+            return f"Let's try again, {name}!"
+    return f"Congratulations, {name}!"
 
 
 def main():
     welcome()
     print(game(yours_name()))
+
 
 if __name__ == '__main__':
     main()
